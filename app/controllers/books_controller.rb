d3c2bf6a5d259dc047772f661ee40a03ce4bd11e
yamaguchishuhe <shuhei0976820@gmail.com>
2021-12-08 04:33:46 +0000

    yonkaime

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,20 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
   
+  def edit
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end 
 end
